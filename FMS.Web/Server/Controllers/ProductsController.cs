9ea7112d7ba0f6c99e89f3d1f261e671323a3077
Raul Marks <rauloelmaro@gmail.com>
2021-05-06 12:28:41 +0300

    Added initial product list page

---
+++ FMS.Web/Server/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using FMS.DAL;
+using FMS.Web.Server.Extensions;
+using FMS.Web.Shared;
+using FMS.Web.Shared.Dtos;
+using FMS.Web.Shared.Options;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FMS.Web.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly FMSContext _context;
+
+        public ProductsController(FMSContext context)
+        {
+            _context = context;
+        }
+
+        //POST: api/products
+        [HttpPost]
+        public async Task<ActionResult<PagedResult<ProductListDto>>> GetProductList(ProductListOptions options)
+        {
+            var query = _context.Products
+                .AsNoTracking();
+
+            return await query
+                .OrderBy(p => p.Code)
+                .Select(p => new ProductListDto
+                {
+                    Id = p.Id,
+                    Code = p.Code,
+                    Name = p.Name
+                })
+                .GetPagedAsync(options.CurrentPage, options.PageSize);
+        }
+    }
+}
---
+++ FMS.Web/Shared/Dtos/Product/ProductListDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FMS.Web.Shared.Dtos
+{
+    public class ProductListDto
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
