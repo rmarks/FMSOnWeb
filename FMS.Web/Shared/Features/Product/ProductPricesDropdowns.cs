7738b941367af0dda7598e0285819b35490d2fb1
Raul Marks <rauloelmaro@gmail.com>
2021-05-25 10:23:56 +0300

    Initial ProductPrices component

---
+++ FMS.Web/Shared/Features/Product/ProductPricesDropdowns.cs
@@ -0,0 +1,10 @@
+﻿using FMS.Web.Shared.Features.Shared;
+using System.Collections.Generic;
+
+namespace FMS.Web.Shared.Features.Product
+{
+    public class ProductPricesDropdowns
+    {
+        public IEnumerable<DropdownDto> PriceLists { get; set; }
+    }
+}
